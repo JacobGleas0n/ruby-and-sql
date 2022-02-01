3c82eb1b203979a49dac01edf6fe15494d6194b1
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-02-01 17:14:39 +0000

    associations lab done

--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -5,6 +5,13 @@
 
 # 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name, e.g.:
 
+
+#puts "Contacts: #{Contact.all.count}"
+#all_contacts = Contact.all
+#for contact in all_contacts
+#    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+#end  
+
 # ---------------------------------
 # Contacts: 4
 # Andy Jassy - andy@amazon.com - Amazon.com, Inc.
@@ -13,6 +20,37 @@
 # Tim Cook - tim@apple.com - Apple Inc.
 
 # 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
+
+for company in Company.all
+    puts company.name
+    for contact in company.contacts
+        puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+    end 
+end
+
+#This is the long way below that doesn't do a double for loop, originally odne in class 
+#apple = Company.where({name:"Apple, Inc."})[0]
+
+#puts "#{apple.name}"
+#for contact in apple.contacts
+#    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+#end  
+
+
+#amazon = Company.where({name:"Amazon.com, Inc."})[0]
+
+#puts "#{amazon.name}"
+#for contact in amazon.contacts
+#    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+#end  
+
+
+#tesla = Company.where({name:"Tesla,Inc."})[0]
+
+#puts "#{tesla.name}"
+#for contact in tesla.contacts    
+#    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+#end  
 
 # ---------------------------------
 # Apple Inc.
